a79e386f36dc1a1b7a058a0d57d008e7620519dd
LeraKryuchkova25 <valeriyakryuchkova25@yandex.ru>
2024-09-22 15:36:18 +0300

    14.2

---
+++ PythonProjects/SeleniumTest/common/conf.py
@@ -0,0 +1,16 @@
+class Cfg:
+    """
+    Configuration class
+    """
+    URL ='https://pokemonbattle-stage.ru'
+    API_URL = 'https://api.pokemonbattle-stage.ru/v2'
+    VALID = {
+        'email': 'lerochka@ya.ru',        # type your valid email
+        'password': 'Iloveqa1'            # type your valid password
+    }
+    INVALID = {
+        'email': 'proverka@mail.net',     # type invalid email
+        'password': 'Iloveqa1'            # type invalid password
+    }
+    TRAINER_ID = 608                      # type your trainer id
+    TRAINER_TOKEN = 'd1723adb38dbd7ab52aca8b3fea889ce'      # type your trainer token
